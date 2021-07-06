6212712e23e1a0d61965cb0074ab5162d54df2f9
mihirpatil0 <mihirrpatil@gmail.com>
2021-07-07 02:00:14 +0530

    [mihir] Added: empty project structure for deck of cards.

---
+++ src/com/bridgelab/deckofcards/DeckOfCards.java
@@ -0,0 +1,14 @@
+package com.bridgelab.deckofcards;
+
+/**************************************************************
+ * @author mihir
+ * 
+ * In this class We are having deck of cards.
+ * Using random function to distribute cards to each player.
+ * Specifying each player should get only 9 cards.
+ **************************************************************/
+
+public class DeckOfCards 
+{
+		
+}
---
+++ src/com/bridgelab/deckofcards/DeckOfCardsMain.java
@@ -0,0 +1,16 @@
+package com.bridgelab.deckofcards;
+
+/******************************************************
+ * @author mihir
+ *	
+ * This class is a main class from here
+ * we will call the methods we are using in the program.
+ ******************************************************/
+
+public class DeckOfCardsMain
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
